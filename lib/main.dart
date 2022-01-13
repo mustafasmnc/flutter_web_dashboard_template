b2891d94c6630b341fa23aca8dddff35b7d6114a
Mustafa <mustafasmnc@gmail.com>
2022-01-13 14:03:35 +0300

    menu controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:web_dashboard/controllers/menu_controller.dart';
 import 'package:web_dashboard/layout.dart';
 
 void main() {
+  Get.put(MenuController());
   runApp(const MyApp());
 }
 
